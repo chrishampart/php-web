853783af4e67da9d241ed52573cfca09eb65038c
champart <chris@clubguide.com>
2014-10-01 15:54:46 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PHP Web</title>
+</head>
+<body>
+This is a php website 
+<br />
+
+<?php
+// prints something like: Wednesday the 15th
+echo date('l \t\h\e jS');
+?>
+
+</body>
+</html>
+
